f684d053dbf29f5367c4a9ff39d40543a2f672b5
Jean-François Grang <jfgrang@2appaz.com>
2021-10-27 15:23:48 +0200

    Harmonized Plan properties

--- src/ios/Hybrid/PLYPlan+Hybrid.m
+++ src/ios/Hybrid/PLYPlan+Hybrid.m
@@ -10,46 +10,67 @@
 @implementation PLYPlan (Hybrid)
 
 - (NSDictionary *)asDictionary {
-  NSMutableDictionary<NSString *, NSObject *> *dict = [NSMutableDictionary new];
+	NSMutableDictionary<NSString *, NSObject *> *dict = [NSMutableDictionary new];
 
-  [dict setObject:self.vendorId forKey:@"vendorId"];
-  [dict setObject:@(self.hasIntroductoryPrice) forKey:@"hasIntroductoryPrice"];
+	[dict setObject:self.vendorId forKey:@"vendorId"];
+	[dict setObject:@(self.hasIntroductoryPrice) forKey:@"hasIntroductoryPrice"];
+	[dict setObject:@([self type]) forKey:@"type"];
 
-  if (self.name != nil) {
-    [dict setObject:self.name forKey:@"name"];
-  }
+	if (self.hasIntroductoryPrice && [[self introAmount] intValue] == 0) {
+		[dict setObject:@(YES) forKey:@"hasFreeTrial"];
+		[dict removeObjectForKey:@"hasIntroductoryPrice"];
+	}
 
-  NSString *price = [self localizedFullPriceWithLanguage:nil];
-  if (price != nil) {
-    [dict setObject:price forKey:@"price"];
-  }
+	if (self.name != nil) {
+		[dict setObject:self.name forKey:@"name"];
+	}
 
-  NSString *amount = [self localizedPriceWithLanguage:nil];
-  if (amount != nil) {
-    [dict setObject:amount forKey:@"amount"];
-  }
+	NSString *price = [self localizedFullPriceWithLanguage:nil];
+	if (price != nil) {
+		[dict setObject:price forKey:@"price"];
+	}
 
-  NSString *period = [self localizedPeriodWithLanguage:nil];
-  if (period != nil) {
-    [dict setObject:period forKey:@"period"];
-  }
+	NSDecimalNumber *amount = [self amount];
+	if (amount != nil) {
+		[dict setObject:amount forKey:@"amount"];
+	}
 
-  NSString *introPrice = [self localizedFullIntroductoryPriceWithLanguage:nil];
-  if (introPrice != nil) {
-    [dict setObject:introPrice forKey:@"introPrice"];
-  }
+	NSDecimalNumber *introAmount = [self introAmount];
+	if (introAmount != nil) {
+		[dict setObject:introAmount forKey:@"introAmount"];
+	}
 
-  NSString *introAmount = [self localizedIntroductoryPriceWithLanguage:nil];
-  if (introAmount != nil) {
-    [dict setObject:introAmount forKey:@"introAmount"];
-  }
+	NSString *currencyCode = [self currencyCode];
+	if (currencyCode != nil) {
+		[dict setObject:currencyCode forKey:@"currencyCode"];
+	}
 
-  NSString *introDuration = [self localizedIntroductoryDurationWithLanguage:nil];
-  if (introDuration != nil) {
-    [dict setObject:introDuration forKey:@"introDuration"];
-  }
-  
-  return dict;
+	NSString *currencySymbol = [self currencySymbol];
+	if (currencySymbol != nil) {
+		[dict setObject:currencySymbol forKey:@"currencySymbol"];
+	}
+
+	NSString *period = [self localizedPeriodWithLanguage:nil];
+	if (period != nil) {
+		[dict setObject:period forKey:@"period"];
+	}
+
+	NSString *introPrice = [self localizedFullIntroductoryPriceWithLanguage:nil];
+	if (introPrice != nil) {
+		[dict setObject:introPrice forKey:@"introPrice"];
+	}
+
+	NSString *introDuration = [self localizedIntroductoryDurationWithLanguage:nil];
+	if (introDuration != nil) {
+		[dict setObject:introDuration forKey:@"introDuration"];
+	}
+
+	NSString *introPeriod = [self localizedIntroductoryPeriodWithLanguage:nil];
+	if (introPeriod != nil) {
+		[dict setObject:introPeriod forKey:@"introPeriod"];
+	}
+
+	return dict;
 }
 
 @end
