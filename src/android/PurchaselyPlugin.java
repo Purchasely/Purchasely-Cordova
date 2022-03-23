09a578642d593b61b5e8fe4a84a5d6e178c991b4
Kevin Herembourg <kevin.herembourg@gmail.com>
2022-03-23 16:00:00 +0100

    feat: add abtestid and abTestVariantId to interceptor info

--- src/android/PurchaselyPlugin.java
+++ src/android/PurchaselyPlugin.java
@@ -602,6 +602,8 @@ public class PurchaselyPlugin extends CordovaPlugin {
             if(info.getContentId() != null) infoMap.put("contentId", info.getContentId());
             if(info.getPresentationId() != null) infoMap.put("presentationId", info.getPresentationId());
             if(info.getPlacementId() != null) infoMap.put("placementId", info.getPlacementId());
+            if(info.getAbTestId() != null) infoMap.put("abTestId", info.getAbTestId());
+            if(info.getAbTestVariantId() != null) infoMap.put("abTestVariantId", info.getAbTestVariantId());
 
             HashMap<String, Object> resultForCordova = new HashMap<>();
             resultForCordova.put("info", infoMap);
